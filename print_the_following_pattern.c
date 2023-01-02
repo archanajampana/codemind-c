99033abc15a5741eaa43fcec5a0d50e5e1ea9d3b
archanajampana <119840726+archanajampana@users.noreply.github.com>
2023-01-02 20:56:24 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {         printf("%d",j);         }         printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,j,c=0;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		for(j=n;j>=1;j--)
-	{
-			printf("%d ",j);
-	}
-	printf("
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+        printf("%d",j);
+        }
+        printf("
 ");
-}
+    }
+    
 }
